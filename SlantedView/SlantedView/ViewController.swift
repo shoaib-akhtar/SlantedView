373bc7a9e34b9d417133d8e2b1106f593ef508b7
Shoaib Akhtar <>
2019-05-23 18:05:09 +0500

    - Initial commit - Demo setup

---
+++ SlantedView/SlantedView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlantedView
+//
+//  Created by Shoaib Akhtar on 23/05/2019.
+//  Copyright © 2019 ShoaibAkhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
